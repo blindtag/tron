b7653588406b23fb05a021baaa6b01d836139277
kenshak <phylace0@gmail.com>
2021-06-30 17:02:21 +0200

    'tron-layout'

--- main.js
+++ main.js
@@ -1,30 +1,16 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const electron = require('electron')
 const path = require('path')
+const url = require('url')
+const {app, BrowserWindow} = electron
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+let mainWindow
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-
-}
-
-app.whenReady().then(() => {
-  createWindow()
-  
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+app.on('ready', function(){
+  mainWindow = new BrowserWindow({})
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'mainWimdow.html'),
+    protocol: 'file',
+    slashes: true
+  }))
 })
